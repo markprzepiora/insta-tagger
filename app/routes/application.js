d962a057dc5ab789d50fc786cba2191ea35ef57d
Mark Przepiora <mark.przepiora@gmail.com>
2017-01-17 09:42:32 -0700

    Working app :+1:

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,40 +1,15 @@
 import Ember from 'ember';
 
+function fetchJSON(url) {
+  return Ember.$.getJSON(url);
+}
+
+function emberCreate(obj) {
+  return Ember.Object.create(obj);
+}
+
 export default Ember.Route.extend({
   model() {
-    return [
-      {
-        label: "Film photography",
-        tags: ["ishootfilm", "filmisnotdead", "believeinfilm", "analogphotography", "analogfilm", "buyfilmnotmegapixels", "filmphoto", "filmshooters", "staybrokeshootfilm", "フィルム", "フィルムカメラ", "フィルム写真"]
-      },
-      {
-        label: "Medium format",
-        tags: ["mediumformat", "mediumformatfilm"]
-      },
-      {
-        label: "35mm",
-        tags: ["35mm", "35mmfilm"]
-      },
-      {
-        label: "Mamiya C220",
-        tags: ["6x6", "mamiyac220"]
-      },
-      {
-        label: "Tri-X",
-        tags: ["trix400", "kodaktrix", "kodaktrix400"]
-      },
-      {
-        label: "B&W",
-        tags: ["monochrome", "bnwphotography"]
-      },
-      {
-        label: "Landscape",
-        tags: ["landscape", "landscapephotography"]
-      },
-      {
-        label: "Calgary",
-        tags: ["calgary", "yyc"]
-      },
-    ].map(obj => Ember.create(obj));
+    return fetchJSON('/tags.json').then(tags => tags.map(emberCreate));
   },
 });
