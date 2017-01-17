97c7d098f05be88fa05a6d63cd431b47206eadf8
Mark Przepiora <mark.przepiora@gmail.com>
2017-01-17 01:36:06 -0700

    Add application

---
+++ app/controllers/application.js
@@ -0,0 +1,26 @@
+import Ember from 'ember';
+
+function flatten(array) {
+  return array.reduce(function(a, b) {
+      return a.concat(b);
+  }, []);
+}
+
+export default Ember.Controller.extend({
+  selectedChoices: Ember.computed.filterBy('model', 'selected', true),
+  _selectedTags: Ember.computed.mapBy('selectedChoices', 'tags'),
+  selectedTags: Ember.computed('_selectedTags.[]', function() {
+    return flatten(this.get('_selectedTags'));
+  }),
+  selectedTagString: Ember.computed('selectedTags.[]', function() {
+    return this.get('selectedTags').map(tag => `#${tag}`).join(" ");
+  }),
+
+  actions: {
+    copy() {
+      const textarea = document.querySelector('.js-tags');
+      textarea.select();
+      document.execCommand('copy');
+    },
+  },
+});
