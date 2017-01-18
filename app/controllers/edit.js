9469962137c28d1b34891bbc6fd9ca5ef416ddc4
Mark Przepiora <mark.przepiora@gmail.com>
2017-01-18 08:13:38 -0700

    Allow defaults to be reloaded and confirm not saving

--- app/controllers/edit.js
+++ app/controllers/edit.js
@@ -3,6 +3,10 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   modelLoader: Ember.inject.service('model-loader'),
 
+  // used to keep track of whether we need to warn the user that they haven't
+  // saved their changes
+  saved: false,
+
   actions: {
     addRow(afterIndex) {
       const beforeIndex = afterIndex + 1;
@@ -17,7 +21,18 @@ export default Ember.Controller.extend({
 
     save() {
       this.get('modelLoader').replaceWith(this.get('model'));
+      this.set('saved', true);
       this.transitionToRoute('index');
+    },
+
+    loadDefault() {
+      const text = "Are you sure? This will overwrite all of your choices with the defaults.";
+      if (window.confirm(text)) {
+        this.set('saved', true);
+        this.get('modelLoader').loadDefault().then(() => {
+          this.transitionToRoute('index');
+        })
+      }
     },
   },
 });
--- app/routes/edit.js
+++ app/routes/edit.js
@@ -6,4 +6,25 @@ export default Ember.Route.extend({
   model() {
     return this.get('modelLoader').find().then(model => model.clone());
   },
+
+  setupController(controller) {
+    this._super(...arguments);
+    controller.set('saved', false);
+  },
+
+  actions: {
+    willTransition(transition) {
+      const saved = this.controller.get('saved');
+      if (saved) {
+        return true;
+      }
+
+      const message = "Are you sure? You haven't saved your changes";
+      if (!window.confirm(message)) {
+        transition.abort();
+      }
+
+      return true;
+    },
+  },
 });
